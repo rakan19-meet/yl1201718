e03f0444c8cbc8919293ae509c1ca966fd0d957b
rakan <rakandarwish3@gmail.com>
2017-11-19 19:22:57 +0200

    anthing

---
+++ lab3/lab5.py
@@ -0,0 +1,17 @@
+from turtle import *
+import turtle
+import.random
+
+colormode(255)
+
+class Square(Turtle):
+	def __init__(self,size):
+		Turtle.__init__(self)
+		self.shapesize(size)
+		self.shape("square")
+	def random_color(self):
+		self.
+
+
+square1 = Square(5)
+turtle.mainloop()
