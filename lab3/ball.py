e926fef71f024d3f5228277cde00d0352b0daf00
rakan <rakandarwish3@gmail.com>
2018-01-21 19:19:28 +0200

    agario

--- lab3/ball.py
+++ lab3/ball.py
@@ -1,79 +1,52 @@
-from turtle import*
+from turtle import *
 import turtle
-import random
 import math
+import random
+colormode(255)
 
-RUNNING = True
-SLEEP = 0.0077
-screen_WIDTH = turtle.getcanvas().winfo_width()/2
-SCREEN_HEIGHT = turtle.getcanvas().winfo_height()/2
+
+running = True
+sleep = 0.0077
+screen_width =int(turtle.getcanvas().winfo_width()/2)
+screen_height = int(turtle.getcanvas().winfo_height()/2)
 
 
 class Ball(Turtle):
-	def __init__ (self,x,y,dx,dy,r,color):
+	def __init__(self, x, y, dx, dy, r, color):
 		Turtle.__init__(self)
 		self.pu()
 		self.goto(x,y)
 		self.dx = dx
 		self.dy = dy
-		self.r  = r
-		turtle.shape("circle")
-		turtle.shapesize(r/10)
-		turtle.color("red")
+		self.r = r
+		self.shape("circle")
+		self.shapesize(r/10)
+		self.color("red")
 
-def move(self, screen_width, screen_hight):
-	current_x = self.xcor()
-	current_y = self.ycor()
-	new_x = current_x + dx
-	new_y = current_y + dy
-	right_side_ball = new_x + r 
-	left_side_ball = new_x - r 
-	top_side_ball = new_x + r 
-	bottom_side_ball = new_y - r 
-	self.goto(new_x , new_y)
+
+
+	def move(self, screen_width, screen_height):
+		current_x = self.xcor()
+		current_y = self.ycor()
+		new_x = current_x + self.dx
+		new_y = current_y + self.dy
+		right_side_ball = new_x + self.r
+		left_side_ball = new_x - self.r
+		top_side_ball = new_y + self.r
+		bottom_side_ball = new_y - self.r
+		self.goto(new_x, new_y)
 	
-	
 
-	if right_side_ball > 300:
-		new_x = current_x - new_x
-	
-	elif bottom_side_ball < -300:
-		new_y = current_y + dy
+		if right_side_ball >= screen_width:
+			new_x = current_x - self.dx
 
-	elif left_side_ball < -300:
-		new_x = current_x + dx
+		elif left_side_ball >= -screen_width:
+			new_x = current_x + self.dx
 
-	elif right_side_ball > 300:
-		new_x = current_x - dx
+		elif top_side_ball >= screen_height:
+			new_y = current_y - self.dy
 
-
-
-#def top(self):
-#	return.self.ycor()+(0.5*self.height)
-
-
-
-#def bottom(self):
-#	return.self.ycor()-(0.5*self.height)
-
-#def right(self):
-#	return.self.xcor()+(0.5*self.width)
-#def left(self):
-#	return.self.xcor()-(0.5*self.width)
-
-# if (A.top() > B.bottom() and A.right() > B.left() and A.bottom() < B.top() and A.left() > B.right()):
-#	return true 
-
-my_ball = Ball(100,0,5,5,10,"red")
-my_ball.goto(10,10)
-my_ball.ht()
-mainloop()
-
-
-
-
-
-
-
+		elif bottom_side_ball >= -screen_height:
+			new_y = current_y + self.dy
 
 
--- lab3/individualprojball.py
+++ lab3/individualprojball.py
@@ -1,12 +1,71 @@
-import turtle
+from turtle import*
 import time
 import random
-from ball import ball
-
+from ball import*
 #turtle.tracer(0)
 #ht()
 
+RUNNING = True 
+SLEEP = 0.0077
+
+SCREEN_WIDTH =int(turtle.getcanvas().winfo_width()/2)
+SCREEN_HEIGHT = int(turtle.getcanvas().winfo_height()/2)
+
+NUMBER_OF_BALLS = 5
+MINIMUM_BALL_RADIUS = 10
+MAXIMUM_BALL_RADIUS = 100
+MINIMUM_BALL_DX = -5
+MAXIMUM_BALL_DX = 5
+MINIMUM_BALL_DY = -5
+MAXIMUM_BALL_DY = 5
 my_ball = Ball (0,0,5,5,10,"red")
+BALLS = []
+
+for i in range(NUMBER_OF_BALLS):
+	x = random.randint (-SCREEN_WIDTH + MAXIMUM_BALL_RADIUS , SCREEN_WIDTH - MAXIMUM_BALL_RADIUS)
+	y = random.randint (-SCREEN_HEIGHT + MAXIMUM_BALL_RADIUS , SCREEN_HEIGHT - MAXIMUM_BALL_RADIUS)
+	dx = random.randint (MINIMUM_BALL_DX , MAXIMUM_BALL_DX)
+	if dx == 0:
+		dx += 1
+	dy = random.randint (MINIMUM_BALL_DY , MAXIMUM_BALL_DY)
+	if dy == 0:
+		dy += 1	
+	radius = random.randint (MINIMUM_BALL_RADIUS , MAXIMUM_BALL_RADIUS)
+	color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+
+ball = Ball(x,y,dx,dy, radius, color)
+BALLS.append(ball)
+
+
+def move_all_BALLS():
+	for variable in range(NUMBER_OF_BALLS):
+		BALLS[variable].move(SCREEN_WIDTH,SCREEN_HEIGHT)
+
+for i in range (100):
+	move_all_BALLS()		
+
+def check_collide(ball_a,ball_b):
+	if ball_a == ball_b:
+		return False
+
+	BALLS_distance = ((ball_A.xcor()-ball_b.xcore())^2+(ball_a.ycor())^2)^0.5
+
+def check_all_BALLS_collision():
+	for ball_a in range (BALLS):
+		for ball_b in range (BALLS):	
+			if check_collide == True:
+				radius1 = ball_a.r
+				radius2 = ball_b.r
+				if radius1 > radius2:
+					ball.b = ball(x,y,dx,dy,radius,color)
+					radius1 += 1
+				elif radius1 < radius2:
+					ball.a = ball(x,y,dx,dy,radius,color)
+					radius2 += 1
+
+
+
+
 
 
 mainloop()
